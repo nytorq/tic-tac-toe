baa14d8cf7ef5a38ed27484e0b8290741bc38b70
nytorq <7298152+nytorq@users.noreply.github.com>
2023-08-01 20:03:48 -0600

    Cleaned up evaluateRows. Handled when to call a win or pass the turn.

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const gameBoard = (function () {
     const columns = 3;
     const rows = 3;
     const board = [];
-    const playerFactory = (name, turn, marker) => {
-        return { name , turn, marker}
+    const players = [
+        {
+            id: 1,
+            marker: 'X',
+        },
+        {
+            id: 2,
+            marker: 'O'
+        }
+    ];
+    
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
 
     for (i=0 ; i < rows ; i++) {
@@ -17,51 +30,49 @@ const gameBoard = (function () {
         }
     }
 
-    console.log('Start of the game board:')
-    console.log(board);
+    const evaluateRows = () => {
+        let marker = activePlayer.marker
+        let winner;
+        // for (i = 0 ; i < board.length ; i++) {
+        //     if ( Object.is(marker, board[i][0]) && Object.is(marker, board[i][1]) && Object.is(marker, board[i][2])) {
+        //         console.log(`Player ${activePlayer.id} has won the game. Found in board[${[i]}] row!`);
+        //     } else if (Object.is(marker, board[0][i]) && Object.is(marker, board[1][i]) && Object.is(marker, board[2][i])) {
+        //         console.log(`Player ${activePlayer.id} has won the game. Found in board[${[i]}] col!`);
+        //     } else if (Object.is(marker, board[0][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[2][2])) {
+        //         console.log(`Player ${activePlayer.id} has won the game. Found in top-left to bottom-right diagonal.`);
+        //     } else if (Object.is(marker, board[2][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[0][2])) {
+        //         console.log(`Player ${activePlayer.id} has won the game. Found in bottom-left to top-right diagonal.`);
+        //     }
+        // }
+
+        for (i = 0 ; i < board.length ; i++) {
+            if ( Object.is(marker, board[i][0]) && Object.is(marker, board[i][1]) && Object.is(marker, board[i][2])) {
+                winner = activePlayer.id;
+            } else if (Object.is(marker, board[0][i]) && Object.is(marker, board[1][i]) && Object.is(marker, board[2][i])) {
+                winner = activePlayer.id;
+            } else if (Object.is(marker, board[0][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[2][2])) {
+                winner = activePlayer.id;
+            } else if (Object.is(marker, board[2][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[0][2])) {
+                winner = activePlayer.id;
+            }
+        }
+
+        if (winner) {
+            console.log(`Player ${activePlayer.id} has won the game.`);
+        } else {
+            switchPlayerTurn();
+            console.log(`Player ${activePlayer.id} it's your turn. Use gameBoard.playRound() to make a move.`)
+        }
+        console.log(board);
+    }    
     
-    const playRound = (position, marker) => {
+    const playRound = (position) => {
         if (board[position[0]][position[1]]) {
             console.log("This cell is already taken. Please choose another.")
         } else {
-            board[position[0]][position[1]] = marker;
+            board[position[0]][position[1]] = activePlayer.marker;
+            evaluateRows();
         }
-        // Evaluate rows
-        for (i = 0 ; i < board.length ; i++) {
-            if ( Object.is(marker, board[i][0]) && Object.is(marker, board[i][1]) && Object.is(marker, board[i][2])) {
-                console.log(`Winner is marker "${marker}", found in board[${[i]}] row!`);
-            }
-
-        }
-        // Evalutate cols
-        for (i = 0 ; i < board.length ; i++) {
-            if ( Object.is(marker, board[0][i]) && Object.is(marker, board[1][i]) && Object.is(marker, board[2][i])) {
-                console.log(`Winner is marker "${marker}", found in board[${[i]}] col!`);
-            }
-        }
-
-        // Evaluating diagonals
-            // Diagonal going from top-left to bottom-right
-            if ( Object.is(marker, board[0][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[2][2]) )  {
-                console.log(`Winner is marker "${marker}", found in top-left to bottom-right diagonal.`);
-            }
-
-            // Diagonal going from bottom-left to top-right
-            if ( Object.is(marker, board[2][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[0][2]) ) {
-                console.log(`Winner is marker "${marker}", found in bottom-left to top-right diagonal.`);
-            }
-
-        console.log(board);
     }
-
-    // const evaluator = () => {
-    //     // Horizontal checks
-    //     for (i = 0 ; i < board.length ; i++) {
-    //         if ( board[i][0] === board[i][1] === board[i][2] ) {
-    //             console.log("Winner found in " + board[i] + " row!")
-    //         }
-    //     }
-    // } 
-
-    return { board, playRound, playerFactory };   
+    return { board, playRound, players };   
 })();
