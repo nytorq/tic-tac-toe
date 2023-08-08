4c92893671e5c5cf106e30a272099047a8c7a6bc
nytorq <7298152+nytorq@users.noreply.github.com>
2023-08-07 21:46:32 -0600

    Stylized everything, fixed clearBoard, improved winning eval and how it stylizes the board

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ const ticTacToe = (function () {
         {
             id: 1,
             marker: 'X',
+            name: ''
         },
         {
             id: 2,
-            marker: 'O'
+            marker: 'O',
+            name: ''
         }
     ];
+    const gameStatus = {
+        winner: '',
+        winningCells: []
+    }
     
     let activePlayer = players[0];
+
+
 
     const switchPlayerTurn = () => {
         ticTacToe.activePlayer = ticTacToe.activePlayer === players[0] ? players[1] : players[0];
@@ -44,38 +52,58 @@ const ticTacToe = (function () {
     createBoard();
 
     const clearBoard = () => {
+        event.preventDefault();
         for (i = 0 ; i < board.length ; i++) {
             for (j = 0 ; j < board.length ; j++) {
                 board[i][j] = '';
             }   
         }
-        console.log("The gameboard has been cleared", '\n', board)
+        let cells = document.getElementsByClassName('gameBoardCell');
+        for (i = 0 ; i < cells.length ; i++) {
+            cells[i].innerHTML = '';
+        }
+        ticTacToe.gameStatus.winner = '';
+        ticTacToe.gameStatus.winningCells = [];
+        console.log("The game has been reset", '\n', board)
     };
 
     const evaluateRows = () => {
-        let marker = activePlayer.marker
-        let winner;
+        let marker = ticTacToe.activePlayer.marker
+        // let winner;
 
         // Check for a winner
         for (i = 0 ; i < board.length ; i++) {
-            if ( Object.is(marker, board[i][0]) && Object.is(marker, board[i][1]) && Object.is(marker, board[i][2])) {
-                winner = activePlayer.id;
-            } else if (Object.is(marker, board[0][i]) && Object.is(marker, board[1][i]) && Object.is(marker, board[2][i])) {
-                winner = activePlayer.id;
-            } else if (Object.is(marker, board[0][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[2][2])) {
-                winner = activePlayer.id;
-            } else if (Object.is(marker, board[2][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[0][2])) {
-                winner = activePlayer.id;
+            if  // Checking rows...
+                (Object.is(marker, board[i][0]) && Object.is(marker, board[i][1]) && Object.is(marker, board[i][2])) {
+                ticTacToe.gameStatus.winner = activePlayer.id;
+                ticTacToe.gameStatus.winningCells.push([parseInt(`${i}`),0], [parseInt(`${i}`),1], [parseInt(`${i}`),2]);
+                ScreenController.displayWin();
+            }   // Checking columns
+                else if (Object.is(marker, board[0][i]) && Object.is(marker, board[1][i]) && Object.is(marker, board[2][i])) {
+                ticTacToe.gameStatus.winner = activePlayer.id;
+                ticTacToe.gameStatus.winningCells.push([parseInt(`${i}`),0], [parseInt(`${i}`),1], [parseInt(`${i}`),2]);
+                ScreenController.displayWin();
             }
         }
+        // Checking top-left to bottom-right diagonal
+        if (Object.is(marker, board[0][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[2][2])) {
+            ticTacToe.gameStatus.winner = activePlayer.id;
+            ticTacToe.gameStatus.winningCells.push([0,0],[1,1],[2,2]);
+            ScreenController.displayWin();
+        }   // Checking bottom-left to top-right diagonal
+            else if (Object.is(marker, board[2][0]) && Object.is(marker, board[1][1]) && Object.is(marker, board[0][2])) {
+            ticTacToe.gameStatus.winner = activePlayer.id;
+            ticTacToe.gameStatus.winningCells.push([2,0],[1,1],[0,2]);
+            ScreenController.displayWin();
+        }
 
-        if (winner) {
-            console.log(`Player ${ticTacToe.activePlayer.id} has won the game.`);
+        if (ticTacToe.gameStatus.winner) {
+            console.log(`Player ${ticTacToe.gameStatus.winner} has won the game.`);
         } else if (turns === 8) {
             console.log('Stalemate!');
         } else {
             switchPlayerTurn();
-            console.log(`Player ${ticTacToe.activePlayer.id} it's your turn. Use ticTacToe.playRound() to make a move.`)
+            console.log(`Player ${ticTacToe.gameStatus.winner} it's your turn. Use ticTacToe.playRound() to make a move.`)
         }
         console.log(board);
     }
@@ -90,7 +118,7 @@ const ticTacToe = (function () {
         }
     }
     
-    return { board, playRound, players, activePlayer, turns, clearBoard};
+    return { board, playRound, players, activePlayer, turns, clearBoard, gameStatus};
 })();
 
 
@@ -106,6 +134,8 @@ const ScreenController = (function() {
     translator.set(7, [2,1]);
     translator.set(8, [2,2]);
     const GameBoard = document.createElement('div');
+    const nameInputs = document.getElementsByClassName('nameInputs')
+    const cells = document.getElementsByClassName('gameBoardCell');
     GameBoard.className = 'gameBoard';
     document.body.appendChild(GameBoard);
     let cell;
@@ -114,14 +144,11 @@ const ScreenController = (function() {
         return cell.target.getAttribute('cell-id');
     }
     const insertMarker = (cell) => {
-        /*
-        - Click event sends up cell id
-        - Transpose cell id to equivalent item in array
-        - Call the playRound() function and pass the above value as an argument
-        */
-        // console.log(getCellID(cell));
         let arrayPosition = translator.get(Number(getCellID(cell)));
         cell.target.innerHTML = ticTacToe.activePlayer.marker;
+        if (ticTacToe.activePlayer.marker == 'O') {
+            cell.target.style.color = '#0093ff';
+        }
         ticTacToe.playRound(arrayPosition)
     }
     for (i = 0 ; i < 9 ; i++) {
@@ -131,5 +158,37 @@ const ScreenController = (function() {
         cell.addEventListener('click', insertMarker);
         GameBoard.appendChild(cell);
     }
-    return {translator}
+
+    const recordPlayerName = (event) => {
+        // console.log(event)
+        // console.log(event.srcElement.id)
+        // console.log(event.target.value);
+        // console.log(event.srcElement.id);
+        if (event.srcElement.id == 'player1Name') {
+            ticTacToe.players[0].name = event.target.value;
+            console.log(`Player 1's name is now ${ticTacToe.players[0].name}`)
+        } else {
+            ticTacToe.players[1].name = event.target.value;
+            console.log(`Player 2's name is now ${ticTacToe.players[1].name}`)
+        }
+    }
+
+    
+    for (i = 0 ; i < 2 ; i++) {
+        nameInputs[i].addEventListener('blur', recordPlayerName)
+    }
+    
+    const displayWin = () => {
+        for (i = 0 ; i < ticTacToe.gameStatus.winningCells.length ; i++) {
+            for (j = 0 ; j < 9 ; j++) {
+                if (ScreenController.translator.get(j).toString() == ticTacToe.gameStatus.winningCells[i].toString()) {
+                    // ScreenController.cells[j].style.border = '3px solid white';
+                    // ScreenController.cells[j].style.boxShadow = 'inset 0 0 20px 9px #00ffa18f';
+                    ScreenController.cells[j].classList.add('winningCell');
+                }
+            }            
+        }
+    }
+    
+    return {translator, nameInputs, displayWin, cells}
 })();
